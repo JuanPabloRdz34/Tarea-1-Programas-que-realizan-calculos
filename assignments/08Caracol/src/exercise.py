2eab9fcb84f82c3b298fd08d4eadbd41128dd140
JuanPabloRdz34 <88863237+JuanPabloRdz34@users.noreply.github.com>
2021-08-23 12:22:07 -0500

    Update exercise.py

--- assignments/08Caracol/src/exercise.py
+++ assignments/08Caracol/src/exercise.py
@@ -1,7 +1,10 @@
 def main():
     #escribe tu código abajo de esta línea
-
-
+    #5.7 mm/s (milímetros por segundo)
+    minutos = float(input('Dame los minutos: '))
+    s = minutos * 60 
+    mm_s = s * 5.7
+    cm = mm_s / 10
     print("Centímentros recorridos:",cm)
 
 
