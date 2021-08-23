bcc84ba265c860f7f84945f1a9b1c2ea18a3eba5
JuanPabloRdz34 <88863237+JuanPabloRdz34@users.noreply.github.com>
2021-08-23 12:26:22 -0500

    Update exercise.py

--- assignments/02AreaTriangulo/src/exercise.py
+++ assignments/02AreaTriangulo/src/exercise.py
@@ -1,6 +1,9 @@
 def main():
     #escribe tu código abajo de esta línea
-    
+    base = int(input('Introduce la base: '))
+    altura = int(input('Introduce la altura: '))
+
+    area = (base * altura) / 2
     print("El área es:",area)
 
 
